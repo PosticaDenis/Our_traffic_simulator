c7b9309dcb942c88e01705852a178ba5923f1d94
Botnari Nicolae <nicolae.botnaru@gmail.com>
2016-04-28 17:03:57 +0300

    BKG ADD

--- Traffic/main.cpp
+++ Traffic/main.cpp
@@ -56,6 +56,59 @@ int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpsz
     return messages.wParam;
 }
 
+
+bool LoadAndBlitBitmap(char* szFileName, HDC hWinDC)
+{
+	// Load the bitmap image file
+	HBITMAP hBitmap;
+	hBitmap = (HBITMAP)LoadImage(NULL, szFileName, IMAGE_BITMAP, 0, 0,LR_LOADFROMFILE);
+	// Verify that the image was loaded
+	if (hBitmap == NULL) {
+		::MessageBox(NULL, TEXT("LoadImage Failed"), TEXT("Error"), MB_OK);
+		return false;
+	}
+
+	// Create a device context that is compatible with the window
+	HDC hLocalDC;
+	hLocalDC = ::CreateCompatibleDC(hWinDC);
+	// Verify that the device context was created
+	if (hLocalDC == NULL) {
+		::MessageBox(NULL, TEXT("CreateCompatibleDC Failed"), TEXT("Error"), MB_OK);
+		return false;
+	}
+
+	// Get the bitmap's parameters and verify the get
+	BITMAP qBitmap;
+	int iReturn = GetObject(reinterpret_cast<HGDIOBJ>(hBitmap), sizeof(BITMAP),
+		reinterpret_cast<LPVOID>(&qBitmap));
+	if (!iReturn) {
+		::MessageBox(NULL, TEXT("GetObject Failed"), TEXT("Error"), MB_OK);
+		return false;
+	}
+
+	// Select the loaded bitmap into the device context
+	HBITMAP hOldBmp = (HBITMAP)::SelectObject(hLocalDC, hBitmap);
+	if (hOldBmp == NULL) {
+		::MessageBox(NULL, TEXT("SelectObject Failed"), TEXT("Error"), MB_OK);
+		return false;
+	}
+
+	// Blit the dc which holds the bitmap onto the window's dc
+	BOOL qRetBlit = ::BitBlt(hWinDC, 0, 0, qBitmap.bmWidth, qBitmap.bmHeight,
+		hLocalDC, 0, 0, SRCCOPY);
+	if (!qRetBlit) {
+		::MessageBox(NULL, TEXT("Blit Failed"), TEXT("Error"), MB_OK);
+		return false;
+	}
+
+	// Unitialize and deallocate resources
+	::SelectObject(hLocalDC, hOldBmp);
+	::DeleteDC(hLocalDC);
+	::DeleteObject(hBitmap);
+	return true;
+}
+
+
 LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     static HDC hdc,hdcMem,hdccMem;
@@ -134,6 +187,8 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
         {
             hdc = BeginPaint(hwnd,&ps);
             GetClientRect(hwnd,&rect);
+
+            LoadAndBlitBitmap(TEXT("map.bmp"), hdc);
 
             hdcMem = CreateCompatibleDC(hdc);
             hbmMem = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
